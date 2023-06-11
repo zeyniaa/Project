325a10fc48e371c850f38f5faa6195ade79d1278
zeyniaa <zeiniahalfiah@gmail.com>
2023-06-12 03:21:09 +0700

    baru

---
+++ admin_page.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal Portfolio Website</title>
+    <link rel="stylesheet" href="css/style2.css">
+</head>
+<body>
+    <div class="hero">
+        <nav>
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">GRAPHIC</a></li>
+                <li><a href="#">HISTORY</a></li>
+            </ul>
+        </nav>
+        <div class="detel" >
+            <h1>I'm <span>Admin</span></h1>
+            <p>This is admin page to organizing this ciwi website
+                <br>Details and work exiprins web development</br>
+            </p>
+        </div>
+        <div>
+            
+        </div>
+
+
+</body>
+
+
+</html>
